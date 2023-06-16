2f54ecb171214b77dc0b062e3827f6335e6ee31d
Eileen Kuehn <eileen.kuehn@kit.edu>
2023-06-16 22:48:45 +0200

    feat: add description for x gate

--- tdot_utils.py
+++ tdot_utils.py
@@ -24,7 +24,10 @@ original_image = load_image(cat_path)
 image = original_image
 pixels_to_transform = []
 quantum_circuit = None
-quantum_cat_output = widgets.Output()
+quantum_cat_circuit = widgets.Output()
+quantum_cat_description = widgets.Output()
+quantum_cat_description.layout.width = "350px"
+quantum_cat_mapping = widgets.Output()
 
 
 def reduce_values(qubits):
@@ -80,14 +83,27 @@ rect = interactive(
 def process_image(option):
     global quantum_circuit
     current_circuit = None
+    description = "Diese Katze wurde nicht durch einen Quantencomputer bearbeitet. Schau dir ruhig auch den Quantenschaltkreis an, dieser sollte leer sein. Es wird also keine Operation, also kein Gate, auf unseren Qubits ausgeführt."
     if option == "Invertierte Katze":
         quantum_circuit = QuantumCircuit(qubits.value)
-        print("setting circuit")
         for i in range(qubits.value):
             quantum_circuit.x(i)
         mapping = create_mapping(quantum_circuit)
         q_image = convert_image(qubits.value, image, mapping, pixels_to_transform)
         current_circuit = quantum_circuit
+        description = """
+Um eine Zahl zu invertieren, multipliziert man sie beispielsweise mit -1. Auf Quantencomputern benutzen wir dafür das sogenannte <i>X-Gate</i>. Das X-Gate sorgt dafür, dass der aktuelle Zustand unseres Qubits invertiert wird.
+Es wird also quasi aus jeder 0 eine 1 gemacht. Das passt ja: auf deinem Circuit befinden sich auf jedem Qubit ein X-Gate, das den Wert ändert. 
+<br/><br/>
+Wahrscheinlich hast du es schon gemerkt, aber Qubits sind für einen Quantencomputer das, was Bits für unsere klassischen Computer sind; Die kleinste Informationseinheit die wir mit diesen Geräten abbilden können.
+Das Spannende ist nun, mit so einem Qubit kann man viel mehr machen, als wir es mit einem klassischen Bit könnte!
+<br/><br/>
+Ein Qubit kann nicht nur entweder 0 oder 1 sein, sondern beides gleichzeitig mit einer gewissen Wahrscheinlichkeit. Klingt ein bisschen komisch?
+<br/><br/>
+Stell dir jetzt einfach vor, im klassischen haben wir nur einen Strich an dessen Endpunkten entweder `0` oder `1` steht. Genau diese und nur diese beide Zustände können wir einnehmen.
+Im Quantencomputing haben wir nun für ein Qubit die Möglichkeit als Zustand jeden erdenklichen Punkt auf der Oberfläche einer Kugel einzunehmen. 
+<img src="images/qbits.png" alt="Bit und Qubit Darstellung"/>
+"""
     elif option == "Vertauschte Katze":
         rgb_qubits = create_rgb_qubits(qubits.value)
         quantum_circuit = QuantumCircuit(*rgb_qubits)
@@ -126,9 +142,15 @@ def process_image(option):
     plt.imshow(q_image)
     plt.show()
 
-    with quantum_cat_output:
-        quantum_cat_output.clear_output()
+    with quantum_cat_circuit:
+        quantum_cat_circuit.clear_output()
         display(current_circuit.draw("mpl"))
+    with quantum_cat_description:
+        quantum_cat_description.clear_output()
+        display(widgets.HTML(value=description))
+    with quantum_cat_mapping:
+        quantum_cat_mapping.clear_output()
+        print_mapping(mapping)
 
 
 quantum_cat = interactive(
