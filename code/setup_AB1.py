0730b1cc3c68d108cb0466cd877274f38c7a888a
Eileen Kuehn <eileen.kuehn@kit.edu>
2026-04-20 23:38:09 +0200

    refactor: finish refactoring python worksheet

--- code/setup_AB1.py
+++ code/setup_AB1.py
@@ -1,5 +1,7 @@
-from template import output
+from template import output, questions
 import template.table_widget as table
+
+multiply = None
 
 
 def setup_erfolg():
@@ -44,3 +46,54 @@ def show_datatype_table():
             return table.WRONG, outputstring
 
     table.show_table("AB1-2a", table_content, column_widths, check_func=check_table)
+
+
+def prompt_1b():
+    questions.prompt_answer(
+        "AB1-2a",
+        input_prompt="Deine Hypothese",
+        input_description="Deine Hypothese zur Frage 1b:",
+    )
+
+
+def check_3a(fn):
+    if fn is not None:
+        for a, b in ((0, 0), (-1, 2), (3, 3), (-10, -2)):
+            if a * b != fn(a, b):
+                output.wrong(
+                    f"Bitte überprüfe deine Implementierung, z.B. für {a} und {b}"
+                )
+                return
+        else:
+            output.correct("Deine Implementierung scheint korrekt. Super!")
+    else:
+        output.wrong(
+            "Bitte erstelle noch deine Implementierung der Funktion `multiply`."
+        )
+
+
+def prompt_4a():
+    check_func = questions.contains_check(
+        ["Zeile 5", "Zeile 10", "Zeile 13", "Zeile 16", "Zeile 21"],
+        equals_ignore_case=False,
+        every_solution_present=True,
+    )
+    questions.prompt_answer_with_check(
+        "AB1-4a",
+        input_prompt="Deine Antwort",
+        input_description="Deine Antwort für die if Abfragen:",
+        check_func=check_func,
+        success_message="Korrekt! Du hast die korrekten if Abfragen identifiziert.",
+        error_message="Deine Antwort enthält noch nicht alle Zeilen, die ausgegeben werden.",
+    )
+
+
+def prompt_6a():
+    questions.prompt_answer_with_check(
+        "AB1-6a",
+        input_prompt="Deine Antwort",
+        input_description="Deine Antwort für die Ausgabe:",
+        check_func=lambda value: value == "[1, 2, 3, 4, 5]",
+        success_message="Korrekt! Genau das wird ausgegeben.",
+        error_message="Das stimmt so nicht ganz. Denk nochmals darüber nach oder probier es gern selbst mal aus.",
+    )
